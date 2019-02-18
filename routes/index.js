6d30c9339a046c7d7fcf4ad9250b44e5e9ca4077
kate kramer <kate@katemariekramer.com>
2019-02-18 16:40:30 -0600

    register route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Kate is Awesome' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'registration' });
+});
+
 module.exports = router;
