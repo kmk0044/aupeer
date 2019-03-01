f55be70a5eb6d395497235cafa91477dc471056f
kate kramer <kate@katemariekramer.com>
2019-03-01 10:58:37 -0600

    modify server to require https
    kate kramer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 var path = require('path');
-var server = require('http').createServer(app);
+var server = require('https').createServer(app);
 var io = require('socket.io').listen(server);
 var hbs = require( 'express-handlebars' );
 const mysql = require('mysql'); 
